e10da809d72a01fb51ef42bfb9451875d110e2fc
Daniel Yuschick <daniel.yuschick@supermetrics.com>
2022-01-18 14:20:48 +0200

    chore(flex) update flex test

--- packages/flex/src/Flex.test.tsx
+++ packages/flex/src/Flex.test.tsx
@@ -7,7 +7,11 @@ import { render, screen } from '../../utils/test-utils';
 describe('<Flex />', () => {
   test('Renders with correct testId', () => {
     const testId = 'flex-test-id';
-    render(<Flex testId={testId}>Kaakao UI</Flex>);
+    render(
+      <Flex justifyContent="center" testId={testId}>
+        Kaakao UI
+      </Flex>
+    );
     screen.getByTestId(testId);
   });
 });
